9cb015fce4328da9b3225f58d65b13d258e9068c
NirajTiwari7 <nirajtiwari0716@gmail.com>
2022-12-10 12:01:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+
 import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React , { useState } from 'react';
+import Alert from './components/Alert';
+
 
 function App() {
+ 
+  const [mode, setMode] = useState('light'); // WHEATHER MODE IS ENABLE OR NOT
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message ,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+    // apna aap 2second bad showalert chala jaya ga
+  }
+
+  const toggleMode = ()=>{
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#0d0d0d';
+      showAlert("Dark mode has been enabled" , "success")
+     //  document.title = 'TextUtils - Dark Mode';
+     
+     // title change ho jaya ga dark mode enable karna sa
+
+      // == [ JAISA KOI KOI WEBSITE MAI BLINK HOTA RAHA HAI KOI IMAGE YA TEXT KI PHONE MAI VIRUS HAI ETC ] { MAI BHI AISA HI KUCH KARNA CHAHTA PAR MAI TITLE KO BLINK KAR VANA CHAHTA HO  }
+      /*
+      setInterval(() => {
+        document.title = 'TextUtils is Amazing Mode';
+      }, 2000);
+      setInterval(() => {
+        document.title = 'Install TextUtils Now';
+      }, 4000);
+      */
+    }
+     else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled" , "success")
+     // document.title = 'TextUtils - Light Mode'; 
+     
+     // -- title change ho jaya ga light mode enable karna sa
+
+      
+      // == [ JAISA KOI KOI WEBSITE MAI BLINK HOTA RAHA HAI KOI IMAGE YA TEXT KI PHONE MAI VIRUS HAI ETC ] { MAI BHI AISA HI KUCH KARNA CHAHTA PAR MAI TITLE KO BLINK KAR VANA CHAHTA HO  }
+      /*
+      setInterval(() => {
+        document.title = 'TextUtils is Amazing Mode';
+      }, 2000);
+
+      setInterval(() => {
+        document.title = 'Install TextUtils Now';
+      }, 4000);
+      */
+    }
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <> 
+   
+
+   <Navbar  title="TextUtils"  abouttext="TextUtils About" mode={mode} toggleMode={toggleMode} />
+
+   <Alert alert={alert} />
+
+   <About />
+
+   <div className="container my-3">
+   <TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />
+
+   </div>
+    
+    
+
+ </>
   );
 }
 
 export default App;
+
+
+
